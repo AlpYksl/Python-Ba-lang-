afd27a1c1885af38dafedbebff5a783832467d60
AlpYksl <56409177+AlpYksl@users.noreply.github.com>
2019-11-13 22:09:57 +0300

    Solutiouns

---
+++ 3and5multiSum.py
@@ -0,0 +1,6 @@
+# 1000 e kadar olan 3 ve 5 ile bölünebilen sayılar
+def multiples_of_3_or_5():
+    for number in range(1000):
+        if not number % 3 or not number % 5:
+            yield number
+print (sum(multiples_of_3_or_5()))
